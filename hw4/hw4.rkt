0605697698f02ec35e41546527be1855c765383e
aaimnr <amiran.chyb@gmail.com>
2013-02-27 23:42:37 +0100

    hw4 11/11

--- hw4/hw4.rkt
+++ hw4/hw4.rkt
@@ -6,9 +6,9 @@
 ;; put your code below
 
 (define (sequence low high stride)
-    (if (> low high)
-        null
-        (cons low (sequence  (+ low stride) high stride))))
+  (if (> low high)
+      null
+      (cons low (sequence  (+ low stride) high stride))))
 
 (define (string-append-map xs suffix)
   (map (λ (s) (string-append s suffix)) xs))
@@ -26,14 +26,14 @@
 
 (define funny-number-stream 
   (letrec ([f (λ (x) (cons 
-                       (if (= (remainder x 5) 0)
-                           (* -1 x)
-                           x)
-                       (λ () (f (+ x 1)))))])
-   (λ()(f 1))))
+                      (if (= (remainder x 5) 0)
+                          (* -1 x)
+                          x)
+                      (λ () (f (+ x 1)))))])
+    (λ()(f 1))))
 
 (define dan-then-dog (λ () (cons "dan.jpg" (λ () ( cons "dog.jpg" dan-then-dog )))))
- 
+
 (define (stream-add-zero s)
   (define pr (s))
   (λ () (cons (cons 0 (car pr))
@@ -44,7 +44,7 @@
     (cons 
      (cons (list-nth-mod xs n) 
            (list-nth-mod ys n))
-          (λ () (f (+ n 1)))))
+     (λ () (f (+ n 1)))))
   (λ () (f 0)))
 
 (define (vector-assoc v vec)
@@ -56,12 +56,12 @@
            ]
           [#t (v-ass (+ n 1))]))
   (v-ass 0))
-    
-                
-  
-  (define (cached-assoc xs n)
-    (letrec ([cur-pos 0]
-      [memo (make-vector n #f)])
+
+
+
+(define (cached-assoc xs n)
+  (letrec ([cur-pos 0]
+           [memo (make-vector n #f)])
     (λ (v) 
       (let ([ans (vector-assoc v memo)])
         (if ans
@@ -71,21 +71,32 @@
                 (vector-set! memo cur-pos (cons v new-ans))
                 (set! cur-pos (remainder (+ cur-pos 1) n))
                 new-ans)))))))
-    
-     
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
 
 
-
-  
+(define-syntax while-less
+  (syntax-rules (do)
+    [(while-less e1 do e2)
+     (letrec ([e1m e1]
+              [e2m (λ()
+                       (let ([e2eval e2])
+                         (if (< e2eval e1m)
+                         (e2m)
+                         #t)))])
+       (e2m))]))
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
